adedf7d7ca7aba51b7cbe418f938033c688a6608
Asyurazhr <Asyuradjola2004@gmail.com>
2025-11-20 19:10:15 +0700

    Add composable un tuk TampilSiswaKt

---
+++ app/src/main/java/com/example/prak8/view/uicontroller/TampilSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.prak8.view.uicontroller
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.prak8.R
+import com.example.prak8.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
