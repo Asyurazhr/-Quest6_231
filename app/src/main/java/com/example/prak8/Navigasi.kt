4ff7f061ca15178f5e63d9f7e6623b657b2faf45
Asyurazhr <Asyuradjola2004@gmail.com>
2025-11-20 19:16:51 +0700

    Add composable di navigasikt

---
+++ app/src/main/java/com/example/prak8/Navigasi.kt
@@ -0,0 +1,32 @@
+package com.example.prak8
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.prak8.view.uicontroller.FormSiswa
+import com.example.prak8.view.uicontroller.TampilSiswa
+import com.example.prak8.viewmodel.SiswaViewModel
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+// list id string jenis kelamin
+val JenisK = listOf(
+    R.string.laki,
+    R.string.wanita
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
