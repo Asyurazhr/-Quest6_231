900d62f1841a87581f713b13657216f4cdf98d5f
Asyurazhr <Asyuradjola2004@gmail.com>
2025-11-20 18:58:26 +0700

    Add Composable di file Fromsiswa

---
+++ app/src/main/java/com/example/prak8/view/uicontroller/FormSiswa.kt
@@ -0,0 +1,18 @@
+package com.example.prak8.view.uicontroller
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.prak8.R
+import com.example.prak8.model.Siswa
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
